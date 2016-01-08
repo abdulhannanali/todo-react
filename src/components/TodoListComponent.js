8b29575b408a00fa039c1957baaac6531798ca9f
Abdul Hannan Ali <abdulhannanali@outlook.com>
2016-01-08 10:07:24 +0500

    add TodoListComponent, also manages state for the todos contains all the events regarding the todos

--- src/components/TodoListComponent.js
+++ src/components/TodoListComponent.js
@@ -1,21 +1,116 @@
 var React = require("react")
 var Todos = require("./Todos")
+var TodoForm = require("./TodoForm")
 
-var todosData = [
-  {description: "Take a bath", done: false},
-  {description: "Blah!", done: false},
-  {description: "Why?", done: false}
-]
+class TodoListComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.addTodo = this.addTodo.bind(this)
+    this.removeTodo = this.removeTodo.bind(this)
+    this.checkTodo = this.checkTodo.bind(this)
+    this.checkAllTodos = this.checkAllTodos.bind(this)
+    this.editTodoDescription = this.editTodoDescription.bind(this)
+    this.deleteAllTodos = this.deleteAllTodos.bind(this)
+  }
 
-var TodoListComponent = React.createClass({
-  render: function () {
+  state = {
+    todos: [],
+    todoId: 1
+  };
+
+  addTodo (todo) {
+    if (todo) {
+      this.state.todos.push({
+        description: todo,
+        done: false,
+        id: this.state.todoId
+      })
+    }
+    this.setState({
+      todos: this.state.todos,
+      todoId: this.state.todoId + 1
+    })
+
+    console.log(this.state.todos)
+  }
+
+  removeTodo(todoId) {
+    if (todoId) {
+      var filteredArray = this.state.todos.filter(function (value, index, array) {
+        if (todoId == value.id) {
+          return false
+
+        }
+        else {
+          return true
+        }
+      })
+
+
+      this.setState({
+        todos: filteredArray
+      })
+    }
+  }
+
+  editTodoDescription(todoId, newDescription) {
+    if (todoId && description) {
+      var editedArray = this.state.todos.map(function (value) {
+        if (todoId == value.id) {
+          value.description = newDescription
+        }
+
+        return value
+      })
+
+      this.setState({
+        todos: editedArray
+      })
+    }
+  }
+
+  checkTodo(todoId) {
+    console.log(todoId)
+    if (todoId) {
+      var checkedArray = this.state.todos.filter(function (value, index, array) {
+        if (todoId == value.id) {
+          value.done = !value.done
+        }
+
+        return value
+      })
+
+      this.setState({todos: checkedArray})
+    }
+  }
+
+  checkAllTodos(done) {
+    var checkedArray = this.state.todos.map(function (value, index, array) {
+      value.done = done
+      return value
+    })
+
+    this.setState({todos: checkedArray})
+  }
+
+  deleteAllTodos () {
+    this.setState({
+      todos: []
+    })
+  }
+
+  render () {
     return (
       <div>
-        <Todos todos={todosData}/>
+        <TodoForm addTodo={this.addTodo}/>
+        <Todos todos={this.state.todos}
+               removeTodo={this.removeTodo}
+               checkTodo={this.checkTodo}
+               checkAllTodos={this.checkAllTodos}
+               deleteAllTodos={this.deleteAllTodos}/>
       </div>
     )
   }
-})
-
+}
 
 module.exports = TodoListComponent
